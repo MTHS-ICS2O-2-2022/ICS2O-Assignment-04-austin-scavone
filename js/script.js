5bf1a1fe2ab9b217a3f357964cb9b6329c526ac6
austin <124389297+austin-scavone@users.noreply.github.com>
2023-05-01 16:45:38 +0000

    check

--- js/script.js
+++ js/script.js
@@ -3,3 +3,28 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+'use strict'
+
+/**
+* This function is calculates the pay of the guitars
+*/
+'use strict'
+
+
+
+
+function calculate () {
+// input
+const radius= parseFloat(document.getElementById('radius').value)
+
+
+
+
+// process
+
+
+const volume= (4/3) * Math.PI * (radius ** 3)
+
+
+// output
+document.getElementById('volume').innerHTML = 'volume is: ' + volume + ' mm³'
